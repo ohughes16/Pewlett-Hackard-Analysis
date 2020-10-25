0afec6b894bb606d0d8b25906e9ab3da2a8647b5
ohughes16 <ohughes16@gmail.com>
2020-10-25 15:30:09 -0700

    added sql files from exercise

--- schema.sql
+++ schema.sql
@@ -42,6 +42,14 @@ CREATE TABLE Dept_Emp (
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
-	PRIMARY KEY (emp_no)
+	FOREIGN KEY (emp_no) REFERENCES salaries (emp_no)
+);
+
+CREATE TABLE titles (
+	emp_no INT NOT NULL,
+	title VARCHAR NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (emp_no) REFERENCES salaries, (emp_no),
 );
