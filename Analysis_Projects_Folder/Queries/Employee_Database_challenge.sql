e9a9fdcff1bb864b0c7a39673d643a7312b3d886
ohughes16 <ohughes16@gmail.com>
2020-10-26 22:42:09 -0700

    Finished Challenge code

--- Analysis_Projects_Folder/Queries/Employee_Database_challenge.sql
+++ Analysis_Projects_Folder/Queries/Employee_Database_challenge.sql
@@ -34,3 +34,19 @@ FROM unique_titles as ut
 GROUP BY ut.title
 ORDER BY COUNT DESC;
 
+--DELIVERABLE 2 Mentorship Program
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+    e.first_name,
+    e.last_name,
+    e.birth_date,
+    de.from_date,
+    de.to_date,
+    t.title
+INTO mentorship_eligibility
+FROM employees as e
+    INNER JOIN dept_emp AS de
+        ON (e.emp_no = de.emp_no)
+    INNER JOIN titles as t
+        ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no, de.to_date ASC;
